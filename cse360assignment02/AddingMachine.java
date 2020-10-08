57b2f42479921a87de9e7da78c878a7ab46bea45
Purvesh Biyani <purvesh@Purveshs-MacBook-Pro-3.local>
2020-10-07 21:56:02 -0700

    adding for the second time

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -1,26 +1,59 @@
 package cse360assignment02;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class AddingMachine {
     private int total;
-
+    Queue< String> q = new LinkedList<>() ;
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
+        String str = total +"";
+        q.add(str);
     }
 
     public int getTotal () {
-        return 0;
+        return this.total;
     }
 
     public void add (int value) {
+        String str = " + "+value;
+        q.offer(str);
+        this.total = this.total + value;
     }
 
     public void subtract (int value) {
+        String str = " - "+value;
+        q.offer(str);
+        this.total = this.total - value;
     }
 
     public String toString () {
-        return "";
+        String str = "";
+        for(String s : q){
+            str += s;
+        }
+        return str;
     }
 
     public void clear() {
+        this.total =0;
+        q.clear();
+        String str = 0+"";
+        q.offer(str);
+
+    }
+    public static void main(String[] args){
+        AddingMachine myCalculator = new AddingMachine();
+        myCalculator.add (4);
+        myCalculator.subtract (2);
+        myCalculator.add(5);
+        System.out.println(myCalculator.toString());
+        myCalculator.clear();
+        myCalculator.add (4);
+        myCalculator.subtract (2);
+        myCalculator.add(5);
+        System.out.println(myCalculator.toString());
+
+
     }
 }
