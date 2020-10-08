229b8807f91bf8e8d4f2ee266512f31fedc7d591
Purvesh Biyani <purvesh@Purveshs-MacBook-Pro-3.local>
2020-10-07 22:00:57 -0700

    adding for the third  time

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -42,18 +42,4 @@ public class AddingMachine {
         q.offer(str);
 
     }
-    public static void main(String[] args){
-        AddingMachine myCalculator = new AddingMachine();
-        myCalculator.add (4);
-        myCalculator.subtract (2);
-        myCalculator.add(5);
-        System.out.println(myCalculator.toString());
-        myCalculator.clear();
-        myCalculator.add (4);
-        myCalculator.subtract (2);
-        myCalculator.add(5);
-        System.out.println(myCalculator.toString());
-
-
-    }
 }
